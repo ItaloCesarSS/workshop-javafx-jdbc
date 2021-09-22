f0b0f869225ba3c08e5b94ccd2a0cb648037495d
Italo Cesar <italocesar.ss@hotmail.com>
2021-09-21 21:22:12 -0300

    Saving a new Deparment

--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -14,113 +14,38 @@ import db.DbIntegrityException;
 import model.dao.DepartmentDao;
 import model.entities.Department;
 
-public class DepartmentDaoJDBC implements DepartmentDao{
+public class DepartmentDaoJDBC implements DepartmentDao {
 
 	private Connection conn;
 	
-	public DepartmentDaoJDBC (Connection conn) {
+	public DepartmentDaoJDBC(Connection conn) {
 		this.conn = conn;
 	}
 	
 	@Override
-	public void insert(Department obj) {
-		PreparedStatement st = null;
-		try {
-			st = conn.prepareStatement(
-					"INSERT INTO department "
-					+ "(Id, Name) "
-					+ "VALUES "
-					+ "(?, ?) ",
-					Statement.RETURN_GENERATED_KEYS);
-			st.setInt(1, obj.getId());
-			st.setString(2, obj.getName());
-			
-			int rowsAffected = st.executeUpdate();
-			
-			if (rowsAffected > 0) {
-				ResultSet rs = st.getGeneratedKeys();
-				if (rs.next()) {
-					int id = rs.getInt(1);
-					obj.setId(id);
-				}
-				DB.closeResultSet(rs);
-			}
-			else {
-				throw new DbException ("Unexpected error! No rows affected! ");
-				
-			}
-		}
-		catch (SQLException e) {
-			System.out.println(e.getMessage());
-		}
-		
-	}
-
-	@Override
-	public void update(Department obj) {
-		PreparedStatement st = null;
-		try {
-			st = conn.prepareStatement(
-				"UPDATE department "
-				+ "SET Name = ? "
-				+ "WHERE Id = ? ");
-			st.setInt(2, obj.getId());
-			st.setString(1, obj.getName());
-			st.executeUpdate();
-			}
-		catch (SQLException e) {
-			System.out.println(e.getMessage());
-		}
-		finally {
-			DB.closeStatement(st);
-		}
-		
-	}
-
-	@Override
-	public void deleteById(Integer id) {
-		PreparedStatement st = null;
-		try {
-			st = conn.prepareStatement(
-				"DELETE FROM department WHERE Id = ?");
-
-			st.setInt(1, id);
-
-			st.executeUpdate();
-		}
-		catch (SQLException e) {
-			throw new DbIntegrityException(e.getMessage());
-		} 
-		finally {
-			DB.closeStatement(st);
-		}
-		
-	}
-
-	@Override
 	public Department findById(Integer id) {
-		ResultSet rs = null;
 		PreparedStatement st = null;
-				try {
-					st = conn.prepareStatement(
-					"SELECT * FROM department WHERE Id = ?");
-					st.setInt(1, id);
-					rs = st.executeQuery();
-					if (rs.next()) {
-						Department obj = new Department();
-						obj.setId(rs.getInt("Id"));
-						obj.setName(rs.getString("Name"));
-						return obj;
-					}
-					return null;
-				}
-				catch (SQLException e) {
-					throw new DbException(e.getMessage());
-				}
-				finally {
-					DB.closeStatement(st);
-					DB.closeResultSet(rs);
-				}
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement(
+				"SELECT * FROM department WHERE Id = ?");
+			st.setInt(1, id);
+			rs = st.executeQuery();
+			if (rs.next()) {
+				Department obj = new Department();
+				obj.setId(rs.getInt("Id"));
+				obj.setName(rs.getString("Name"));
+				return obj;
+			}
+			return null;
+		}
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
 	}
 
 	@Override
@@ -150,6 +75,79 @@ public class DepartmentDaoJDBC implements DepartmentDao{
 			DB.closeResultSet(rs);
 		}
 	}
-		
-	
+
+	@Override
+	public void insert(Department obj) {
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement(
+				"INSERT INTO department " +
+				"(Name) " +
+				"VALUES " +
+				"(?)", 
+				Statement.RETURN_GENERATED_KEYS);
+
+			st.setString(1, obj.getName());
+
+			int rowsAffected = st.executeUpdate();
+			
+			if (rowsAffected > 0) {
+				ResultSet rs = st.getGeneratedKeys();
+				if (rs.next()) {
+					int id = rs.getInt(1);
+					obj.setId(id);
+				}
+			}
+			else {
+				throw new DbException("Unexpected error! No rows affected!");
+			}
+		}
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} 
+		finally {
+			DB.closeStatement(st);
+		}
+	}
+
+	@Override
+	public void update(Department obj) {
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement(
+				"UPDATE department " +
+				"SET Name = ? " +
+				"WHERE Id = ?");
+
+			st.setString(1, obj.getName());
+			st.setInt(2, obj.getId());
+
+			st.executeUpdate();
+		}
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} 
+		finally {
+			DB.closeStatement(st);
+		}
+	}
+
+	@Override
+	public void deleteById(Integer id) {
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement(
+				"DELETE FROM department WHERE Id = ?");
+
+			st.setInt(1, id);
+
+			st.executeUpdate();
+		}
+		catch (SQLException e) {
+			throw new DbIntegrityException(e.getMessage());
+		} 
+		finally {
+			DB.closeStatement(st);
+		}
+	}
 }
